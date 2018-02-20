161d2efffe67c7eb58d67363287367d8bc66a4ba
Charles Tapley Hoyt <cthoyt@gmail.com>
2018-02-20 11:12:15 +0100

    Update entrez tests

--- tests/constants.py
+++ tests/constants.py
@@ -10,6 +10,8 @@ from bio2bel_entrez import Manager
 log = logging.getLogger(__name__)
 
 dir_path = os.path.dirname(os.path.realpath(__file__))
+gene_info_test_path = os.path.join(dir_path, 'gene_info')
+homologene_test_path = os.path.join(dir_path, 'homologene.data')
 
 
 class TemporaryCacheClsMixin(unittest.TestCase):
@@ -37,7 +39,10 @@ class PopulatedDatabaseMixin(TemporaryCacheClsMixin):
         """Creates a persistent database and populates it with the test data included in the Bio2BEL Entrez repository
         """
         super(PopulatedDatabaseMixin, cls).setUpClass()
-        cls.manager.populate()
+        cls.manager.populate(
+            gene_info_url=gene_info_test_path,
+            homologene_url=homologene_test_path
+        )
 
     @classmethod
     def tearDownClass(cls):
